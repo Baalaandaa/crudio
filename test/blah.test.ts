f08dd31649fceec926ba53312ed76950ae2c8cea
ruinername <russhianhere@gmail.com>
2021-05-03 20:04:31 +0300

    Update dependencies

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,22 +1,23 @@
-import {Crudio, DBField, Resource} from "../src/index"
+import {Crudio, DBField, Resource} from "../src/index";
+import { describe, expect, it } from "@jest/globals";
 
 describe('sample', () => {
   it('works', () => {
-    
+
       @Resource("test")
       class test{
-      
+
         @DBField("lol")
         lol: string;
         kek: number;
-      
+
         constructor() {
           this.lol = "kek";
           this.kek = 0;
         }
-      
+
       }
-    
+
       let t = new test();
       expect(t.lol).toBe("kek");
       let l = new Crudio([t]);
